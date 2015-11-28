338ae6d9ced7b51cc49e43ec52aab9ecfdcc5fb2
Greg Smith <crashingwaves@fea.st>
2015-11-28 10:39:11 -0700

    Passed jslint

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,101 +1,108 @@
-function calculator(){
-	return {
-		inputVal: "",
-		latestEntry: [],
-		operatorBoolean: true,
-		periodBoolean: false,
-		canUndo: false,
-		appendToInput: function(input){			
-			input = input.toString();
-			this.inputVal += input;
-			this.latestEntry.push(input);
-			this.updateDOM();				
-		},	
-		undo: function(){
-			if(this.canUndo){		
-				var latestEntryLength = this.latestEntry.length - 1,
-					latestEntry = this.latestEntry[latestEntryLength],
-					lengthDifference = this.inputVal.length - latestEntry.length;
+(function () {
+    'use strict';
 
-					if(this.latestEntry[latestEntryLength] === "."){
-						this.periodBoolean = false;
-					} else if(this.isOperator(this.latestEntry[latestEntryLength])){
-						this.operatorBoolean = false;
-					}
-					this.inputVal = this.inputVal.slice(0, lengthDifference);
-					this.latestEntry.pop();
-					if(latestEntryLength === 0){
-						this.canUndo = false;
-					}
-			}
-		},
-		isOperator: function(input){
-			if(input === '+' ||input === "-" ||input === '*' ||input === '/'){
-				return true;
-			} else {
-				return false;
-			}
-		},
-		clear:function(){
-			this.inputVal = "";
-			this.latestEntry = [];
-		},
-		equals: function(){
-			this.inputVal = eval(this.inputVal);
-			this.inputVal = this.inputVal.toString();
-		},
-		setupDOM: function(){
-			var _this = this;
+    function calculator() {
+        return {
+            inputVal: "",
+            latestEntry: [],
+            operatorBoolean: true,
+            periodBoolean: false,
+            canUndo: false,
+            appendToInput: function (input) {
+                input = input.toString();
+                this.inputVal += input;
+                this.latestEntry.push(input);
+                this.updateDOM();
+            },
+            undo: function () {
+                if (this.canUndo) {
+                    var latestEntryLength = this.latestEntry.length - 1,
+                        lastEntry = this.latestEntry[latestEntryLength],
+                        lengthDifference = this.inputVal.length - lastEntry.length;
 
-			$(".numKey").click(function(){
-				_this.operatorBoolean = false;
-				_this.canUndo = true;
-				$('.undo').removeClass('disabled');
-				_this.appendToInput($(this).html());
-			});
-			$(".operator").click(function(){
-				if(_this.operatorBoolean){
-					return
-				} else {
-					_this.operatorBoolean = true;
-					_this.appendToInput($(this).html());
-					_this.periodBoolean = false;
-				}
-			});
-			$(".period").click(function(){
-				if(_this.periodBoolean){
-					return
-				} else{
-					_this.periodBoolean = true;
-					_this.appendToInput($(this).html());
-				}
-			});
+                    if (lastEntry === ".") {
+                        this.periodBoolean = false;
+                    } else if (this.isOperator(this.latestEntry[latestEntryLength])) {
+                        this.operatorBoolean = false;
+                    }
+                    this.inputVal = this.inputVal.slice(0, lengthDifference);
+                    this.latestEntry.pop();
+                    if (latestEntryLength === 0) {
+                        this.canUndo = false;
+                    }
+                }
+            },
+            isOperator: function (input) {
+                if (input === '+' || input === "-" || input === '*' || input === '/') {
+                    return true;
+                } else {
+                    return false;
+                }
+            },
+            clear: function () {
+                this.inputVal = "";
+                this.latestEntry = [];
+            },
+            equals: function () {
+                this.inputVal = eval(this.inputVal);
+                this.inputVal = this.inputVal.toString();
+            },
+            setupDOM: function () {
+                var _this = this;
 
-			$('.clear').click(function(){
-				_this.clear();
-				_this.periodBoolean = false;
-				_this.operatorBoolean = true;
-				_this.updateDOM();
-			});
+                $(".numKey").click(function () {
+                    _this.operatorBoolean = false;
+                    _this.canUndo = true;
+                    $('.undo').removeClass('disabled');
+                    _this.appendToInput($(this).html());
+                });
+                $(".operator").click(function () {
+                    if (_this.operatorBoolean === true) {
+                        return;
+                    } else {
+                        _this.operatorBoolean = true;
+                        _this.appendToInput($(this).html());
+                        _this.periodBoolean = false;
+                    }
+                });
+                $(".period").click(function () {
+                    if (_this.periodBoolean === true) {
+                        return;
+                    } else {
+                        _this.periodBoolean = true;
+                        _this.appendToInput($(this).html());
+                    }
+                });
 
-			$(".undo").click(function(){
-				_this.undo();
-				_this.updateDOM();
-			});
-			$(".equals").click(function(){
-				if(_this.inputVal){
-					_this.equals();
-					_this.updateDOM();
-					_this.canUndo = false;
-					$('.undo').addClass('disabled');
-				}
-			});
-		},
-		updateDOM: (function(){
-			return function(){
-				$('#inputScreen').html(this.inputVal);
-			}
-		})()
-	}
+                $('.clear').click(function () {
+                    _this.clear();
+                    _this.periodBoolean = false;
+                    _this.operatorBoolean = true;
+                    _this.updateDOM();
+                });
+
+                $(".undo").click(function () {
+                    _this.undo();
+                    _this.updateDOM();
+                });
+                $(".equals").click(function () {
+                    if (_this.inputVal) {
+                        _this.equals();
+                        _this.updateDOM();
+                        _this.canUndo = false;
+                        $('.undo').addClass('disabled');
+                    }
+                });
+            },
+            updateDOM: (function () {
+                return function () {
+                    $('#inputScreen').html(this.inputVal);
+                };
+            }())
+        };
 }
 
+var calc = calculator();
+    calc.setupDOM();
+
+}());
